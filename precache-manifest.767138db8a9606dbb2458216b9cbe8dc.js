bdda6fc184ac6bff82be054b75b2821db8d79a1c
Miloš Petrović <milosptr@hotmail.com>
2018-12-28 14:54:42 +0100

    Add files via upload

---
+++ precache-manifest.767138db8a9606dbb2458216b9cbe8dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56b767bce61ac1b0e771",
+    "url": "/static/js/main.56b767bc.chunk.js"
+  },
+  {
+    "revision": "debb835ad8f0d2372444",
+    "url": "/static/js/1.debb835a.chunk.js"
+  },
+  {
+    "revision": "56b767bce61ac1b0e771",
+    "url": "/static/css/main.b55e1f90.chunk.css"
+  },
+  {
+    "revision": "d229dbe1812a0950bb351e726c3f30e5",
+    "url": "/index.html"
+  }
+];
